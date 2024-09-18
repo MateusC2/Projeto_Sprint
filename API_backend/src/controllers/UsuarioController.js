27881e5574175cd9c2c8b7e02b5a63144b8e3493
Arthur16800 <arthur08coutinho@gmail.com>
2024-09-18 16:14:59 -0300

    Definiçao de rotas usuario

---
+++ API_backend/src/controllers/UsuarioController.js
@@ -0,0 +1,21 @@
+module.exports = class UserController{
+
+// Função para cadastro POST
+
+static async CadastroUsauario(req, res){
+    const{}
+}
+
+
+// Função para login POST
+
+// função para alteração
+
+// Função para exclusão
+
+
+
+
+
+
+}
